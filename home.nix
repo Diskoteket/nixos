ad2c654d639b1e3398b14cf7b238b84d64fe7b90
Tim Wetterek <tim@wetterek.se>
2024-05-16 10:31:52 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,60 +1,20 @@
-{ config, pkgs, ... }:
-
-# let
-#   home-manager = builtins.fetchTarball {
-#         url = "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
-#     };  
-# in
-
-{
-  imports = [
-      ./zsh.nix 
-      ./config.nix
-      ./vim.nix
-  ];
-
-  home.username = "mathias";
-  home.homeDirectory = "/home/mathias";
+{ config, pkgs, ... }: {
+  
   home.stateVersion = "23.11";
 
-  home.packages = with pkgs; [
-    vim
-    virtualbox
-    vagrant
-    packer
-    ansible
-    vscode
-    slack
-    git
-    gh
-    google-chrome
-    zsh
-    blackbox-terminal
-    unzip
-    wget
-    gnupg
-    htop
-    btop
-    tailscale
-    docker
-    minecraft
-    dropbox
-    kubectl
-    minikube
-    lazygit
-    todoist
-    spotify
-    teams-for-linux
-    cmake
-    libgit2
-    rustup
-    lsd
-    bat
-  ];
-
-  home.file = {
-    "./oh-my-zsh/themes/bullet-train.zsh-theme".source = ./sources/bullet-train.zsh-theme;
+  # Allow unfree pkgs
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
   };
 
-  programs.home-manager.enable = true;
+  # Import other configs
+  imports = [
+    ./zsh.nix
+    ./packages.nix
+    ./programs.nix
+    ./dconf.nix 
+  ];
+
 }
